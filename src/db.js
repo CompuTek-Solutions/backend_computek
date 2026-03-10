dcd405e8c3e9a995c591706b35d167be1e7637c9
NGUENGA JACKSON <jnguenga@gmail.com>
2026-03-10 13:16:36 +0100

    ajout de vercel

--- src/db.js
+++ src/db.js
@@ -11,6 +11,7 @@ const pool = new Pool({
   host: process.env.DB_HOST || 'localhost',
   port: process.env.DB_PORT || 5432,
   database: process.env.DB_NAME || 'computekSolutions',
+  ssl: process.env.SSL_MODE === 'require' ? { rejectUnauthorized: false } : false,
 });
 
 pool.on('error', (err) => {
